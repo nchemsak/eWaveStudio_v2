c2d67d5b67aaeb6e91a341d6dc19342288da260d
Nicholas Chemsak <Nicholas Chemsak>
2017-05-25 15:25:41 -0500

    fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,34 @@ $(".addChannel").click(function() {
                 <input type="checkbox">
             </label>
         </form>
+
+        <a href="#" class="dropdown-toggle" data-toggle="modal" data-target="#myModal" aria-expanded="false">
+                <img src="images/fx_1.png" class="fx" />
+            </a>
+            <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">effects</h4>
+                        </div>
+                        <div class="modal-body">
+                            <ul>
+                                <li><a href="#">Delay</a></li>
+                                <li><a href="#">Flanger</a></li>
+                                <li><a href="#">Tremolo</a></li>
+                                <li><a href="#">Distortion</a></li>
+                                <li><a href="#">Reverb</a></li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                 <svg aria-hidden="true" height="16" viewBox="0 0 8 16" width="8">
