9eb8bf0392ec4ee509ee40df9895f7b63cd7a518
Nicholas Chemsak <nchemsak@gmail.com>
2017-06-20 10:42:02 -0500

    updated navbar choices

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,7 +17,7 @@ $(".addChannel").click(function() {
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">effects</h4>
+                            <h4 class="modal-title content-head">effects</h4>
                         </div>
                         <div class="modal-body">
                             <ul>
