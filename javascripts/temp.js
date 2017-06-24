084c9519a584baf8d4ef481d9023764ded7c6dc1
Nick Chemsak <nchemsak@gmail.com>
2017-06-23 23:43:21 -0500

    pad styling updates

--- javascripts/temp.js
+++ javascripts/temp.js
@@ -17,7 +17,7 @@ $(".addChannel").click(function() {
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">effects</h4>
+                            <h4 class="modal-title content-head">effects</h4>
                         </div>
                         <div class="modal-body">
                             <ul>
@@ -43,80 +43,30 @@ $(".addChannel").click(function() {
                 </svg>
             </a>
             <ul class="dropdown-menu">
-                <li><a href="#">Load Sample...</a></li>
+                <li><input id="load" name="load" type="file" accept="audio/*"><label for="load">Load Sample...</label></li>
+                <li><a href="#">Select Sample...</a></li>
                 <li role="separator" class="divider"></li>
                 <li><a href="#">Settings</a></li>
                 <li role="separator" class="divider"></li>
-                <li><a data-href="/delete.php?id=54" data-toggle="modal" data-target="#confirm-delete">Delete Channel</a></li>
-
-
-
-
-
+                <li><a href="#">Delete Channel</a></li>
             </ul>
         </li>
-        <span class="instrument-label">
-            <strong><input type="text" class="form-control" id="" /></strong>
-        </span>
-        <div class="pad column_0"></div>
-        <div class="pad column_1"></div>
-        <div class="pad column_2"></div>
-        <div class="pad column_3"></div>
-        <div class="pad column_4"></div>
-        <div class="pad column_5"></div>
-        <div class="pad column_6"></div>
-        <div class="pad column_7"></div>
-        <div class="pad column_8"></div>
-        <div class="pad column_9"></div>
-        <div class="pad column_10"></div>
-        <div class="pad column_11"></div>
-        <div class="pad column_12"></div>
-        <div class="pad column_13"></div>
-        <div class="pad column_14"></div>
-        <div class="pad column_15"></div>
 
-
-
-    <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Confirm Delete</h4>
-                </div>
-
-                <div class="modal-body">
-                    <p>You are about to delete one track, this procedure is irreversible.</p>
-                    <p>Do you want to proceed?</p>
-                    <p class="debug-url"></p>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-danger btn-ok">Delete</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    </div>
-
-    <script>
-        $('#confirm-delete').on('show.bs.modal', function(e) {
-            $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
-
-            $('.debug-url').html('Delete URL: <strong>' + $(this).find('.btn-ok').attr('href') + '</strong>');
-        });
-    </script>
-
-
-    `);
+        <div class="column_0 list-item"></div>
+        <div class="column_1 list-item"></div>
+        <div class="column_2 list-item"></div>
+        <div class="column_3 list-item"></div>
+        <div class="column_4 list-item"></div>
+        <div class="column_5 list-item"></div>
+        <div class="column_6 list-item"></div>
+        <div class="column_7 list-item"></div>
+        <div class="column_8 list-item"></div>
+        <div class="column_9 list-item"></div>
+        <div class="column_10 list-item"></div>
+        <div class="column_11 list-item"></div>
+        <div class="column_12 list-item"></div>
+        <div class="column_13 list-item"></div>
+        <div class="column_14 list-item"></div>
+        <div class="column_15 list-item"></div>
+    </div>`);
 });
-
-
-
-
-
-
-
